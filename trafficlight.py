a725c29c2a469dc7f2f6dad559e56d527b662fb1
Taha Enayat <ta.enayat@gmail.com>
2024-05-03 11:57:00 +0200

    traffic light codes with drafts

--- trafficlight.py
+++ trafficlight.py
@@ -51,3 +51,46 @@ def backward_car_wait_time_calculate(n, time_lag ,red, green, drive_time, nudge=
         if i < 0:
             break
     return wait_time
+
+
+# red = 10 # red time
+# green = 12 # green time
+# time_lag = 0 # lag between traffic lights
+# n = 2 # number of traffic lights
+
+# is_green = np.ones(n, dtype=bool) # by default every traffic light is green
+# x = 0 # total time behind redlight
+# # forward car:
+# for t in range(red+green):
+#     for i in range(n):
+#         if not is_green[i]:
+#             x += t - red
+
+
+# for i in range(n):
+#     if time_lag * i > red:
+#         is_green[i] = True
+
+        # if t_one_cycle == -green + time_lag * i:
+        #     is_green[i] = False
+        # if t_one_cycle == time_lag * i:
+        #     is_green[i] = True
+        # if t_one_cycle == green + time_lag * i:
+        #     is_green[i] = False
+
+
+
+# for t in range(2*(red+green)):
+
+    # if status[i] == False:
+    #     wait_time += 1
+    # else:
+    #     i += 1
+
+
+
+# forward_wait, backward_wait = np.zeros((10,20)), np.zeros((10,20))
+# for red in np.arange(1,11):
+#     for time_lag in np.arange(red+green):
+#         forward_wait[red-1,time_lag] = forward_car_wait_time_calculate(n, time_lag, red, green, drive_time)
+#         backward_wait[red-1,time_lag] = backward_car_wait_time_calculate(n, time_lag, red, green, drive_time)
